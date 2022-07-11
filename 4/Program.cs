f0a6781fe15139eab30294245d7ddd84b5c1eee7
Kseniya <keusy@yandex.ru>
2022-07-11 17:45:25 +0300

    add 4

---
+++ 4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число : ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = 1;
+
+if (b == 1)
+{
+    b += 1;
+}
+
+while (b <= a);
+{
+    Console.Write(b+",");
+    b += 2;
+}
