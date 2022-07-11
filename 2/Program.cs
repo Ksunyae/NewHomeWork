e6b198a11da0af08e1985a61c215ebe65a75d2f4
Kseniya <keusy@yandex.ru>
2022-07-11 16:50:12 +0300

    add 2

---
+++ 2/Program.cs
@@ -0,0 +1,28 @@
+﻿int a, b, c;
+Console.Write("Введите любое число: ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите любое число, отличное от предидуего: ");
+b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите любое число, отличное от двух предидущих: ");
+c = Convert.ToInt32(Console.ReadLine());
+
+if(a > b && a > c)
+{
+    Console.Write("max = ");
+    Console.WriteLine(a);
+    
+}
+if(b > a && b > c)
+{
+    Console.Write("max = ");
+    Console.WriteLine(b);
+    
+}
+if(c > b && c > b)
+{
+    Console.Write("max = ");
+    Console.WriteLine(c);
+    
+}
+
+
