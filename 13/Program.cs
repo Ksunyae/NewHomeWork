86e6acf90ae946a0cc1083ccf8607a036516da65
Kseniya <keusy@yandex.ru>
2022-07-16 16:24:17 +0300

    add

---
+++ 13/Program.cs
@@ -0,0 +1,12 @@
+﻿int a;
+Console.WriteLine("Введите любую цифру обозначающую день недели:");
+a = Convert.ToInt32(Console.ReadLine());
+if (a == 6 || a == 7)
+{
+    Console.WriteLine("Этот день является выходным");
+};
+
+if (a < 6)
+{
+    Console.WriteLine("Это день будний");
+};
