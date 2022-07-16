fbefb59a30e79e92c680bc6585e83ffc4fe9914e
Kseniya <keusy@yandex.ru>
2022-07-16 16:12:10 +0300

    add 12

--- 12/Program.cs
+++ 12/Program.cs
@@ -1,10 +1,13 @@
 ﻿int a;
 Console.WriteLine("Введите любое число:");
 a = Convert.ToInt32(Console.ReadLine());
-if (a < 99);
+if (a <= 99);
 {
-    Console.WriteLine("Третьей цыфры нет");
-    Console.WriteLine("Введите любое число:");
-    a = Convert.ToInt32(Console.ReadLine());
-}
-Console.Write(Convert.ToString(a)[2]);
+    Console.WriteLine("Третьей цифры нет");
+ 
+};
+else
+{
+    Console.Write(Convert.ToString(a)[2]);
+};
+
