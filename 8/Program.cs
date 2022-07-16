cf70c4a9f876d3da933043241e113633d76a22b3
Kseniya <keusy@yandex.ru>
2022-07-16 13:39:47 +0300

    add 8

---
+++ 8/Program.cs
@@ -0,0 +1,10 @@
+﻿int a, b;
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+if (a % b == 0);
+
+else
+{
+    Console.WriteLine("Остаток при де");
+    Console.WriteLine(a % b);
+}
