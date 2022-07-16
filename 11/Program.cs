a4de929c68679a88433833d89cf01d7dc399d907
Kseniya <keusy@yandex.ru>
2022-07-16 15:23:47 +0300

    add 11

---
+++ 11/Program.cs
@@ -0,0 +1,9 @@
+﻿int a;
+Console.WriteLine("Введите любое трехзначное число:");
+a = Convert.ToInt32(Console.ReadLine());
+while (a < 100 && a  > 999)
+{
+    Console.WriteLine("Ты ошибся...");
+    a = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine(a / 10 % 10);
