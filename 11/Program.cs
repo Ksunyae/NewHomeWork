4ab29d46986853c1e918cd423c18142b28a148fe
Kseniya <keusy@yandex.ru>
2022-07-16 15:46:11 +0300

    add 12

--- 11/Program.cs
+++ 11/Program.cs
@@ -1,7 +1,7 @@
 ﻿int a;
 Console.WriteLine("Введите любое трехзначное число:");
 a = Convert.ToInt32(Console.ReadLine());
-while (a < 100 && a  > 999)
+while (a < 100 || a  > 999)
 {
     Console.WriteLine("Ты ошибся...");
     a = Convert.ToInt32(Console.ReadLine());
---
+++ 12/Program.cs
@@ -0,0 +1,10 @@
+﻿int a;
+Console.WriteLine("Введите любое число:");
+a = Convert.ToInt32(Console.ReadLine());
+if (a < 99);
+{
+    Console.WriteLine("Третьей цыфры нет");
+    Console.WriteLine("Введите любое число:");
+    a = Convert.ToInt32(Console.ReadLine());
+}
+Console.Write(Convert.ToString(a)[2]);
